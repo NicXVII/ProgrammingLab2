f18bf755ce25dd008cba028385e6feabf72397ab
NicXVII <diminich.nicolas@volta.ts.it>
2025-03-27 10:50:38 +0100

    finiti secondi es

--- test.py
+++ test.py
@@ -13,9 +13,32 @@ import numpy as np
 file = np.genfromtxt("data.csv", delimiter=',', skip_header=1)
 years = file[:,0]
 years = np.unique(years)
-print(years)
+#print(years)
+year_data = {}
 for year in years:
-    #print(year)
-    year_data = file[file[:,0] == year]
+
+    year_data[year] =  file[file[:,0] == year]
+    #print(year, year_data)
    
-#print(year_data)
+countYear = {}
+sumYear = {}
+sumYearSotto = {}
+print(len(year_data))
+for year in year_data:
+    countYear[year] = len(year_data[year][year_data[year][:,1] > 18])
+    sum_val = np.sum(year_data[year][year_data[year][:,1] > 18, 1])
+    #print(sum_val)
+    sumYear[year] = sum_val / countYear[year] if countYear[year] > 0 else 0
+
+    #print(year, countYear[year])
+
+for year in sorted(countYear.keys()):
+    print("Anno: ", year, "Emissioni sopra 18: ", countYear[year], "Media: ", sumYear[year])
+    
+sumTot = 0
+for year in sumYear:
+    sumTot += sumYear[year]
+
+
+print("Somma totale sopra la soglia: ", sumTot)
+print("Frazione totale sopra la soglia: ", sumTot / np.sum(file[file[:,1] >=0, 1]))
